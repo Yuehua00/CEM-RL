dedfa9aa8ec6e30fda74cafbaaa6c316563b603f
yuehua00 <jennifer50716@gmail.com>
2025-06-22 20:18:53 +0800

    importance mixing

--- EA/ES.py
+++ EA/ES.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import math
+from scipy.stats import norm
 
 from config import args
 from .EA_utils import phenes_to_genes, gene_to_phene
@@ -23,7 +25,7 @@ class CEM:
         self.damp_limit = torch.tensor(1e-5).to(torch.float32).detach().to(args.device)
         self.damp_tau = torch.tensor(0.95).to(torch.float32).detach().to(args.device)
 
-        self.actor_cov_discount = torch.tensor(args.CEM_cov_discount_actor).to(torch.float32).detach().to(args.device)
+        # self.actor_cov_discount = torch.tensor(args.CEM_cov_discount_actor).to(torch.float32).detach().to(args.device)
 
 
     def get_init_actor_population(self, mu_actor: Actor) -> list[Actor]:
@@ -67,7 +69,7 @@ class CEM:
             cov = torch.matmul(self.actor_parents_weights , cov)  # (1 , actor_parents_size) * (actor_parents_size , params_size) -> (1 , params_size)
             cov += self.damp                                      # shape = (1 , params_size)
 
-            cov = self.actor_cov_discount * cov                   # shape = (1 , params_size)
+            # cov = self.actor_cov_discount * cov                   # shape = (1 , params_size)
 
             self.damp = self.damp_tau * self.damp + (1 - self.damp_tau) * self.damp_limit
 
@@ -91,3 +93,170 @@ class CEM:
 
 
         return actor_population[:offspring_size]
+
+
+class CEM_IM:
+
+    def __init__(self):
+
+        self.mu_actor = None
+        self.cov = None
+
+        self.old_mu = None
+        self.old_cov = None
+
+        self.actor_parents_size = int(args.population_size * args.CEM_parents_ratio_actor)
+        self.actor_parents_weights = torch.tensor([np.log((self.actor_parents_size + 1) / i) for i in range(1, self.actor_parents_size + 1)])
+        self.actor_parents_weights /= self.actor_parents_weights.sum()
+        self.actor_parents_weights = self.actor_parents_weights.unsqueeze(dim=0)
+        self.actor_parents_weights = self.actor_parents_weights.to(torch.float32).detach().to(args.device)
+
+        self.damp = torch.tensor(1e-3).to(torch.float32).detach().to(args.device)
+        self.damp_limit = torch.tensor(1e-5).to(torch.float32).detach().to(args.device)
+        self.damp_tau = torch.tensor(0.95).to(torch.float32).detach().to(args.device)
+
+    def old_log_pdf(self, sample):
+
+        sample = phenes_to_genes([deepcopy(sample)])
+        
+        return (
+            -0.5 * torch.log(2 * torch.tensor(math.pi))
+            - torch.log(self.old_cov.sqrt())
+            - 0.5 * ((sample - self.old_mu) / self.old_cov.sqrt()) ** 2
+        ).sum()
+
+    def new_log_pdf(self, sample):
+
+        sample = phenes_to_genes([deepcopy(sample)])
+        
+        return (
+            -0.5 * torch.log(2 * torch.tensor(math.pi))
+            - torch.log(self.cov.sqrt())
+            - 0.5 * ((sample - self.mu_actor) / self.cov.sqrt()) ** 2
+        ).sum()
+    
+    def sample_one_actor(self, actor):
+        
+        param_size = self.mu_actor.numel()
+        epsilon = torch.randn(param_size, dtype=torch.float32, device=args.device)
+        individual = (self.mu_actor + epsilon * self.cov.sqrt()).view(-1)
+
+        ctr = 0
+        for param in actor.parameters():
+
+            n = param.numel()
+
+            param.data.copy_(individual[ctr : ctr + n].view(param.shape))
+
+            ctr += n
+
+        actor.fitness = None
+        
+        return actor
+
+
+    def get_init_actor_population(self, mu_actor: Actor) -> list[Actor]:
+
+        self.mu_actor =  phenes_to_genes([mu_actor]).squeeze(0)  # shape = (params_size,)
+        params_size = self.mu_actor.numel()
+
+        self.cov = (args.CEM_sigma_init) * torch.ones(params_size, dtype=torch.float32, device=args.device)
+
+        self.old_mu = self.mu_actor
+        self.old_cov = self.cov
+        
+        epsilon_half = torch.randn(args.population_size // 2, params_size, dtype=torch.float32, device=args.device)
+        epsilon = torch.cat([epsilon_half, -epsilon_half], dim=0)
+
+        new_gene = self.mu_actor + epsilon * self.cov.sqrt()
+
+        actor_population: list[Actor] = []
+
+        for i in range(args.population_size):
+
+            actor = deepcopy(mu_actor)
+            actor = gene_to_phene(actor, new_gene[i])
+
+            actor_population.append(actor)
+
+        return actor_population
+
+
+    def variate(self, actor_population: list[Actor], offspring_size: int) -> list[Actor]:
+
+        with torch.no_grad():
+
+            actor_population = deepcopy(actor_population)
+            actor_population.sort(key=lambda actor: actor.fitness, reverse=True)
+
+            genes = phenes_to_genes(actor_population)  # shape = (population_size , params_size)
+
+            self.old_mu = self.mu_actor.unsqueeze(0)  # shape = (1, params_size)
+            self.mu_actor = torch.matmul(self.actor_parents_weights, genes[: self.actor_parents_size]).squeeze(0)  # (1 , actor_parents_size) * (actor_parents_size , params_size) -> (1 , params_size) -> (params_size,)
+
+            self.cov = genes[ : self.actor_parents_size] - self.old_mu      # shape = (actor_parents_size , params_size)
+            self.cov = self.cov.pow(2)                                      # shape = (actor_parents_size , params_size)
+            self.cov = torch.matmul(self.actor_parents_weights , self.cov)  # (1 , actor_parents_size) * (actor_parents_size , params_size) -> (1 , params_size)
+            self.cov += self.damp                                      # shape = (1 , params_size)
+
+            # cov = self.actor_cov_discount * cov                   # shape = (1 , params_size)
+
+            self.damp = self.damp_tau * self.damp + (1 - self.damp_tau) * self.damp_limit
+
+            # importance mixing
+            n_resued = 0
+            n_sampled = 0
+
+            idx_reused = []
+            old_samples: list[Actor] = actor_population
+
+            new_population: list[Actor] = [None] * args.population_size
+
+            for i in range(args.population_size):
+
+                if n_resued + n_sampled < args.population_size:
+
+                    sample = old_samples[i]
+
+                    # u = torch.rand(1)
+                    u = np.random.uniform(0, 1)
+                    if np.log(u) < self.new_log_pdf(sample) - self.old_log_pdf(sample):
+                        
+                        new_population[n_resued] = sample
+                        idx_reused.append(i)
+                        n_resued += 1
+                
+                if n_resued + n_sampled < args.population_size:
+
+                    new_sample = self.sample_one_actor(deepcopy(sample))
+
+                    u = np.random.uniform(0, 1)
+                    if np.log(u) >= self.old_log_pdf(new_sample) - self.new_log_pdf(new_sample):
+                        
+                        new_population[-n_sampled-1] = new_sample
+                        n_sampled += 1
+
+                if n_resued + n_sampled >= args.population_size:
+
+                    break
+
+            cpt = n_resued + n_sampled
+            while cpt < args.population_size:
+
+                new_sample = self.sample_one_actor(deepcopy(old_samples[0]))
+                new_population[cpt - n_sampled] = new_sample
+                cpt += 1
+
+            self.old_mu = self.mu_actor
+            self.old_cov = self.cov
+
+            print(f"n_reused: {n_resued}")
+
+            return new_population
+                    
+
+
+
+                
+
+            
